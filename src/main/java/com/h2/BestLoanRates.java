2f5bca76ca1b3a0103a3c4d81b81fb04836ff4c5
MarioKratafila <42386123+MarioKratafila@users.noreply.github.com>
2022-09-21 21:19:14 +0200

    Update BestLoanRates.java

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -8,8 +8,9 @@ public class BestLoanRates {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your name\n");
+        System.out.print("Enter your name ");
         String name = scanner.nextLine();
+        System.out.println();
         System.out.println("Hello " + name);
         System.out.println("Enter the loan term (in years)");
         int loanTermInYears = scanner.nextInt();
