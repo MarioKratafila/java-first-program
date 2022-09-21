fccd46c9eb8a5f403621b160e5d65eb1515f1944
MarioKratafila <42386123+MarioKratafila@users.noreply.github.com>
2022-09-21 21:25:27 +0200

    fix

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -8,17 +8,16 @@ public class BestLoanRates {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your name ");
+        System.out.print("Enter your name\n");
         String name = scanner.nextLine();
-        System.out.println();
-        System.out.println("Hello " + name);
-        System.out.println("Enter the loan term (in years)");
+        System.out.print("Hello " + name + "\n");
+        System.out.print("Enter the loan term (in years)\n");
         int loanTermInYears = scanner.nextInt();
         float bestRate = getRates(loanTermInYears);
         if (bestRate == 0.0f) {
-            System.out.println("No available rates for term: " + loanTermInYears + " years");
+            System.out.print("No available rates for term: " + loanTermInYears + " years\n");
         } else {
-            System.out.println("Best Available Rate: " + bestRate + "%");
+            System.out.print("Best Available Rate: " + bestRate + "%\n");
         }
         scanner.close();
     }
